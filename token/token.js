affcf173ad6fd82184e1c00f2547471ad47b66a8
Glitch (spectacled-ivory-floss) <none>
2021-06-19 14:22:19 +0000

    🎺😚 Checkpoint ./.data/private.pem.txt copy:27641839/3243 ./token/token.js:27641839/24 ./src/server.js:27641839/12

--- token/token.js
+++ token/token.js
@@ -2,7 +2,7 @@ const jsonWebToken = require("jsonwebtoken");
 const fs = require("fs");
 const config = require("../config/config");
 
-const key = fs.readFileSync("./token/private.pem.txt", "utf8");
+const key = fs.readFileSync(".data/private.pem.txt", "utf8");
 const methods = {
   generate: function (clientIp, tenantDomain) {
     // kid and issuer have to match with the IDP config and the audience has to be qlik.api/jwt-login-session
