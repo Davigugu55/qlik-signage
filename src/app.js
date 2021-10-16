a622d60a3f831dc373b790a6f5c7df6481507a73
Glitch (qlik-cloud-jwt) <none>
2021-10-16 20:33:03 +0000

    🍲🚊 Checkpoint ./src/app.js:27641839/124

--- src/app.js
+++ src/app.js
@@ -22,8 +22,11 @@ import { configuration } from './configuration.js'
     }
   });
   
-  const rest = await fetch(`https://${config.tenantDomain}/users/me?
-    qlik-web-integration-id=${config.qlikWebIntegrationId}&qlik-csrf-token=${csrfTokenInfo.headers.get("qlik-csrf-token")}`)
+  const rest = await (await fetch(`https://${config.tenantDomain}/users/me
+  ?qlik-web-integration-id=${config.qlikWebIntegrationId}
+  &qlik-csrf-token=${csrfTokenInfo.headers.get("qlik-csrf-token")}`).then(resp => resp.json()));
+  
+  document.querySelector("#rest").innerHTML= rest;
   
   
 })();
