8fa71a6a5582c1bf73342b4fd01552766aca853a
Glitch (nebula-embed-example) <none>
2021-10-16 13:05:32 +0000

    💂🚁 Checkpoint ./config/config.js:27641839/740

--- config/config.js
+++ config/config.js
@@ -1,7 +1,18 @@
+const loginTypes = {
+  PROVIDE_CREDENTIALS: 'provide-credentials',
+  AUTOMATIC_LOGIN: 'automatic-login'
+}
+
 module.exports = {
-  qlikWebIntegrationId: "xqFZ3rogbF0p7LXX_2lN437Z22a9xHfZ",
+  loginTypes,
+  currentLoginType: loginTypes.AUTOMATIC_LOGIN,
+  
+  // app config 
+  qlikWebIntegrationId: "NjFhqVVmulrrkb30ksf6C-WE4Pl3xxVP",
   tenantDomain: "incognito.us.qlikcloud.com",
-  appId: "66826dee-21db-4de3-8db3-ec9d03aef034",
+  appId: "bc97609f-523b-4fe9-91f6-78aa0bd1b989",
+  
+  // token config
   issuer: "incognito.us.qlikcloud.com",
   keyid: "08c9752e-ba12-482c-85fb-d760c576b6a0"
 };
