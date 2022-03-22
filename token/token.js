b0bbe97284ec08341eceeebe2f1f2f7fd77ec9f2
Glitch (qlik-cloud-jwt) <none>
2022-03-22 16:10:24 +0000

    🎋🚒 Checkpoint ./token/token.js:27641839/193

--- token/token.js
+++ token/token.js
@@ -1,18 +1,20 @@
-const jsonWebToken = require("jsonwebtoken");
+//const jsonWebToken = require("jsonwebtoken");
 const fs = require("fs");
 const config = require("../config/config");
-
+const { v4: uuidv4 } = require("uuid");
 const key = fs.readFileSync(".data/private.key.txt", "utf8");
 const methods = {
   generate: function (sub, name, email, groups = []) {
     // kid and issuer have to match with the IDP config and the audience has to be qlik.api/jwt-login-session
+const Auth = require('@qlik/sdk').default;
 
     const signingOptions = {
-      jti: generateId(8),
+      jti: uuidv4(),
       keyid: config.keyid,
       algorithm: "RS256",
       issuer: config.issuer,
       expiresIn: "30s",
+      notBefore: '0s',
       audience: "qlik.api/login/jwt-session"
     };
 
@@ -27,7 +29,7 @@ const methods = {
       groups: groups
     };
 
-    const token = jsonWebToken.sign(payload, key, signingOptions);
+    const token = Auth.generateSignedToken(payload, key, signingOptions);
     return token;
   }
 };
