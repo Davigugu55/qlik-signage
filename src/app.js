6bc9a0ec648a47e6d150bda74c172d141381ee27
Glitch (qlik-cloud-jwt) <none>
2022-04-27 23:48:02 +0000

    🚲🚕 Checkpoint ./src/authForApp.js:27641839/3125 ./src/app.js:27641839/1154

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
-import { auth } from './auth.js'
+//import { auth } from './auth.js'
 import { configuration } from './configuration.js'
-import { connectQlikApp } from './connectQlikApp.js'
+import *  as config from '../config/config';
+//import { connectQlikApp } from './connectQlikApp.js'
+import Auth, { AuthType } from '@qlik/sdk';
 
 (async () => {
 
-  const { config, csrfTokenInfo } =  await auth()
+  //const { config, csrfTokenInfo } =  await auth()
+  const auth = new Auth({
+    host: 'https://' + config.tenantDomain,
+    authType: AuthType.JWTAuth,
+    webIntegrationId: config.qlikWebIntegrationId,
+    fetchToken: async () => {
+      return await fetch("token").then(resp => resp.json()).then((res) => res.token);
+    }
+  });
   
   //add page content
   let mainTag = document.getElementsByTagName("main");
-  
-  //Embed chart using Nebula.js
+
+//Embed chart using Nebula.js
   // connect to a Qlik Sense application
-  const { app } = await connectQlikApp(config, csrfTokenInfo)
+  await auth.getSessionCookie();
+  // fetch schema for communicating with Qlik's engine API
+  const schema = await fetch("https://unpkg.com/enigma.js/schemas/12.936.0.json").then(response => response.json());
+  const wsUrl = await auth.generateWebsocketUrl(config.appId);
+  
+  const session = window.enigma.create({ schema, url: wsUrl });
+  
+  // 7) open the app
+  const app = await (await session.open()).openDoc(config.appId);
+  
   // create renderer
   const renderer = window.stardust.embed(app, configuration);
   // render toolbar
@@ -30,8 +49,8 @@ import { connectQlikApp } from './connectQlikApp.js'
   
   //embed chart using single API iframe
   let iframeSrc = `https://${config.tenantDomain}/single/?appid=${config.appId}&obj=${config.objId}&opt=ctxmenu,currsel
-  &qlik-web-integration-id=${config.qlikWebIntegrationId}
-  &qlik-csrf-token=${csrfTokenInfo.headers.get("qlik-csrf-token")}`;
+  &qlik-web-integration-id=${config.qlikWebIntegrationId}`;
+  //&qlik-csrf-token=${csrfTokenInfo.headers.get("qlik-csrf-token")}
   
   let iframe = document.createElement("iframe");
   iframe.src = iframeSrc;
---
+++ src/authForApp.js
@@ -0,0 +1,110 @@
+// import { Auth, AuthType } from './qliksdk/qlik.js';
+import Auth, { AuthType } from '@qlik/sdk';
+
+export const authy = async () => {
+  
+  const shouldLoginBox = document.querySelector('#should-login-box')
+  const loginLink = shouldLoginBox.querySelector('a')
+  
+  // 1) get config
+  const { tenantDomain, qlikWebIntegrationId, appId, objId, currentLoginType, loginTypes } = await fetch("config").then((resp) => resp.json());
+  const remoteConfig = { tenantDomain, qlikWebIntegrationId, appId, objId, currentLoginType, loginTypes };
+  // 2) get logged in
+  if(currentLoginType === loginTypes.JWT_LOGIN) await handleAutomaticLogin()
+  else if (currentLoginType === loginTypes.INTERACTIVE_LOGIN) await handleUserLogin()
+
+      // 2.1) login, in order to save some credentials in browser storage
+    //    we are going to need these for next api calls like getting CSRF token
+
+  async function handleAutomaticLogin() {
+    const config = {
+      host: tenantDomain,
+      authType: AuthType.JWTAuth,
+      webIntegrationId: qlikWebIntegrationId,
+      fetchToken: async () => {
+        return await fetch("token").then(resp => resp.json()); 
+        },
+      };
+      async function start(auth) {
+        await auth.getSessionCookie();
+        await auth.rest('/users/me').then((resp) => {
+          console.log(resp);
+        });
+      }
+      try {
+        const auth = new Auth(config);
+        console.log(auth);
+        start(auth);
+      } catch (err) {
+        console.log(err);
+      }
+  }
+  
+  async function handleUserLogin() {
+    const response = await fetch(`https://${tenantDomain}/api/v1/csrf-token`, {
+      credentials: 'include',
+      headers: { 'qlik-web-integration-id': qlikWebIntegrationId }
+    })
+    
+    if(response.status === 401) {
+      shouldLoginBox.style.display = 'block'
+    
+      const loginUrl = new URL(`https://${tenantDomain}/login`);
+      loginUrl.searchParams.append('returnto', window.location.href);
+      loginUrl.searchParams.append('qlik-web-integration-id', qlikWebIntegrationId);
+
+      loginLink.href = loginUrl.href;
+    }
+  }
+  
+  // 3) get CSRF token
+    /*const csrfTokenInfo = await (await fetch(
+    `https://${tenantDomain}/api/v1/csrf-token?qlik-web-integration-id=${qlikWebIntegrationId}`,
+    {
+      credentials: "include",
+      headers: {
+        "Qlik-Web-Integration-ID": qlikWebIntegrationId
+      }
+    }
+  ));*/
+   
+  // 8) if we reached in this step with out any error, try to remove the helper box
+  shouldLoginBox.style.display = 'none'
+  
+  
+  
+  return { remoteConfig/*, csrfTokenInfo */ }
+}
+
+/*
+
+
+
+const config = {
+  host: 'your-tenant',
+  authType: AuthType.JWTAuth,
+  webIntegrationId: 'webIntegration...',
+  fetchToken: () => {
+    return fetch('your_backend/serverless/token')
+      .then((resp) => resp.json())
+      .then((res) => res.token);
+    },
+  };
+  async function start(auth) {
+    await auth.getSessionCookie();
+    await auth.rest('/users/me').then((resp) => {
+      console.log(resp);
+    });
+  }
+  try {
+    const auth = new Auth(config);
+    console.log(auth);
+    start(auth);
+  } catch (err) {
+    console.log(err);
+  }
+
+
+
+
+*/
