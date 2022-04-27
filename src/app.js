83118a593101f1b4521a767e2e4d68f24263a5d1
Glitch (qlik-cloud-jwt) <none>
2022-04-27 23:48:18 +0000

    Rewound to commit 95012a9800f3fde59733ff931598ade0391724a9 + 0 edits

--- src/app.js
+++ src/app.js
@@ -1,36 +1,17 @@
-//import { auth } from './auth.js'
+import { auth } from './auth.js'
 import { configuration } from './configuration.js'
-import *  as config from '../config/config';
-//import { connectQlikApp } from './connectQlikApp.js'
-import Auth, { AuthType } from '@qlik/sdk';
+import { connectQlikApp } from './connectQlikApp.js'
 
 (async () => {
 
-  //const { config, csrfTokenInfo } =  await auth()
-  const auth = new Auth({
-    host: 'https://' + config.tenantDomain,
-    authType: AuthType.JWTAuth,
-    webIntegrationId: config.qlikWebIntegrationId,
-    fetchToken: async () => {
-      return await fetch("token").then(resp => resp.json()).then((res) => res.token);
-    }
-  });
+  const { config, csrfTokenInfo } =  await auth()
   
   //add page content
   let mainTag = document.getElementsByTagName("main");
-
-//Embed chart using Nebula.js
+  
+  //Embed chart using Nebula.js
   // connect to a Qlik Sense application
-  await auth.getSessionCookie();
-  // fetch schema for communicating with Qlik's engine API
-  const schema = await fetch("https://unpkg.com/enigma.js/schemas/12.936.0.json").then(response => response.json());
-  const wsUrl = await auth.generateWebsocketUrl(config.appId);
-  
-  const session = window.enigma.create({ schema, url: wsUrl });
-  
-  // 7) open the app
-  const app = await (await session.open()).openDoc(config.appId);
-  
+  const { app } = await connectQlikApp(config, csrfTokenInfo)
   // create renderer
   const renderer = window.stardust.embed(app, configuration);
   // render toolbar
@@ -49,8 +30,8 @@ import Auth, { AuthType } from '@qlik/sdk';
   
   //embed chart using single API iframe
   let iframeSrc = `https://${config.tenantDomain}/single/?appid=${config.appId}&obj=${config.objId}&opt=ctxmenu,currsel
-  &qlik-web-integration-id=${config.qlikWebIntegrationId}`;
-  //&qlik-csrf-token=${csrfTokenInfo.headers.get("qlik-csrf-token")}
+  &qlik-web-integration-id=${config.qlikWebIntegrationId}
+  &qlik-csrf-token=${csrfTokenInfo.headers.get("qlik-csrf-token")}`;
   
   let iframe = document.createElement("iframe");
   iframe.src = iframeSrc;
